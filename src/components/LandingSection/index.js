3a98303748cc8ac9d61b0c7b105a0454be93b94f
SahelaR <sahelarani1@gmail.com>
2019-06-26 21:48:50 +0100

    root CSS color variables

--- src/components/LandingSection/index.js
+++ src/components/LandingSection/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import CSS from "../LandingSection/LandingSection.module.css"
+import Cover from "/Users/sahel/Documents/portfolioWebsite/src/Assets/Images/cover.png"
 
 export default class LandingSection extends React.Component {
     render() {
         return (
             <div>
-                <p>Landing</p>
+                <div className={CSS.mainCoverContainer}>
+                   
+            </div>
             </div>
         )
     }
