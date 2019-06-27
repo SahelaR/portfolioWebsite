de9d40bfb9c1c0941579fa96130672f07a7bd4be
SahelaR <sahelarani1@gmail.com>
2019-06-27 11:17:38 +0100

    background added to blog section

---
+++ src/components/BlogSection/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CSS from "../BlogSection/BlogSection.module.css"
+
+export default class BlogSection extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className={CSS.mainCoverContainer}>
+                </div>
+            </div>
+        )
+    }
+}
