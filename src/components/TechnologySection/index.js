7ae712185971fdf69827cad4eece89740ad77641
SahelaR <sahelarani1@gmail.com>
2019-06-27 11:02:42 +0100

    background color added to Technology section

--- src/components/TechnologySection/index.js
+++ src/components/TechnologySection/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import CSS from "../TechnologySection/TechnologySection.module.css"
 
 export default class TechnologySection extends React.Component {
     render() {
         return (
             <div>
-                <p>Technology</p>
+                <div className={CSS.mainCoverContainer}>
+
+                </div>
             </div>
         )
     }
