5fded63d4b00d4592fa56495f24233e9e09bea9b
SahelaR <sahelarani1@gmail.com>
2019-07-01 13:19:36 +0100

    media queries added to name

--- src/components/LandingSection/index.js
+++ src/components/LandingSection/index.js
@@ -3,6 +3,9 @@ import CSS from "../LandingSection/LandingSection.module.css"
 import Background from "/Users/sahel/Documents/portfolioWebsite/src/Assets/Images/background.svg"
 
 
+
+
+
 export default class LandingSection extends React.Component {
     render() {
         return (
@@ -11,7 +14,10 @@ export default class LandingSection extends React.Component {
                     backgroundPosition: 'right',
                     backgroundSize: 'cover',
                     backgroundRepeat: 'no-repeat', zIndex: 1, backgroundImage: "url(" + Background + ")" }}>
-                   
+                   <div class={CSS.header}>
+                       <h1 >Hello, I'm Sahela Rani</h1>
+                   </div>
+                
             </div>
             </div>
         )
