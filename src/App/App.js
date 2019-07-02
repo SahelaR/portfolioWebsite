2b25c2e3188bb5f8a2240246fafadbb38fb6679f
SahelaR <sahelarani1@gmail.com>
2019-07-02 10:53:26 +0100

    removed scroll background

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,6 @@ import TechnologySection from "../components/TechnologySection"
 import PortfolioSection from "../components/PortfolioSection"
 import ContactMe from "../components/ContactMe"
 import BlogSection from "../components/BlogSection"
-import ScrollingColorBackground from "react-scrolling-color-background"
 
 
 
@@ -17,38 +16,11 @@ function App() {
 
 <LandingSection />
 <AboutMe />
+<TechnologySection />
+<PortfolioSection />
+<BlogSection />
+<ContactMe />
 
-
-   {/* <ScrollingColorBackground
-        selector='.js-color-stop[data-background-color]'
-        colorDataAttribute='data-background-color'
-        initialRgb='#331832'
-      /> 
-      
-     <section
-        data-background-color='#000000'
-        className='js-color-stop'
-      ><AboutMe /></section>
-
-     <section
-        data-background-color='#331832'
-        className='js-color-stop'
-      ><TechnologySection /> </section>
-
-      <section
-        data-background-color='#000000'
-        className='js-color-stop'
-      ><PortfolioSection /></section>
-
-      <section
-        data-background-color='#331832'
-        className='js-color-stop'
-      ><BlogSection /></section>
-      
-      <section
-        data-background-color='#000000'
-        className='js-color-stop'
-      ><ContactMe /></section>  */}
       </div>
 
   );
