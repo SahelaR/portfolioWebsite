1440db8041a23a37e372d01db0fb4cbc4a37d2eb
SahelaR <sahelarani1@gmail.com>
2019-07-03 11:25:57 +0100

    removed text

--- src/components/LandingSection/index.js
+++ src/components/LandingSection/index.js
@@ -20,19 +20,6 @@ export default class LandingSection extends React.Component {
          </div>
          </div>
             </div>
-            // <div>
-            //     <div className={CSS.mainCoverContainer} style={{
-            //         backgroundPosition: 'right',
-            //         backgroundSize: 'cover',
-            //         backgroundAttachment: 'fixed',
-            //     backgroundRepeat: 'no-repeat', zIndex: 1, backgroundImage: "url(" + Background + ")" }}>
-            //        <div className={CSS.header}>
-            //             <p>front end developer.</p>
-            //            <h1 >Hello, I'm Sahela Rani</h1>
-            //             <p>have a scroll.</p>
-            // </div>
-            // </div>
-            // </div>
         )
     }
 }
