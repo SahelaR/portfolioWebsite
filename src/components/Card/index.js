2b4f85fe5a66426ab6353b5cd7bb4ef50fb4ecda
SahelaR <sahelarani1@gmail.com>
2019-07-05 17:33:30 +0100

    cards mapped over

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,9 +4,10 @@ import CardInfo from "../CardInfo"
 function Card(props) {
     return (
         <div>
-            <img className="" src={props.item.imgSrc} alt={props.item.imgSrc} />
+            <p>Hello</p>
+            {/* <img className="" src={props.item.imgSrc} alt={props.item.imgSrc} />
             {props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link}   />
-            }
+            } */}
         </div>
     )
 }
