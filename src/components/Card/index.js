e236a9852fd3e58b1f5a8c1264ad40e1dece4d25
SahelaR <sahelarani1@gmail.com>
2019-07-05 12:22:24 +0100

    exported card into carousel

---
+++ src/components/Card/index.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+function Card(prop) {
+    return (
+        <div>
+            <img className="" src={props.item.imgSrc} alt={props.item.imgSrc} />
+        </div>
+    )
+}
+
+export default Card;
