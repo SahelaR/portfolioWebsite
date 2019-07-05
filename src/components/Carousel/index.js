9de2f314a61c3e024f54a1cdae7f312f37ff1b84
SahelaR <sahelarani1@gmail.com>
2019-07-05 11:51:07 +0100

    handleclick and state changed added

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Background from "/Users/sahel/Documents/portfolioWebsite/src/Assets/Images/landing.png"
 
-
-
 class Carousel extends React.Component {
     constructor(props) {
         this.state = {
@@ -38,9 +36,30 @@ class Carousel extends React.Component {
 
     }
 
+    handleCardClick = (id, card ) => {
+        let items = {...this.state.items};
+
+        item[id].selected = item[id].selected ? false : true;
+
+        items.ForEach(item => {
+            if(item.id !== id) {
+                item.selected = false;
+            }
+        });
+    }
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} onClick={(e => this.handleCardClick(item.id.e))} key={item.id} />
+        })
+    }
+ 
+
     render() {
         return (
             <p>Projects</p>
         )
     }
 }
+
+export default Carousel
