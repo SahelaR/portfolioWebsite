2cb63ec72ca098e65de69f57fe8c5e08180ecdcd
SahelaR <sahelarani1@gmail.com>
2019-07-05 12:32:17 +0100

    props added to card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,6 +4,8 @@ function Card(prop) {
     return (
         <div>
             <img className="" src={props.item.imgSrc} alt={props.item.imgSrc} />
+            {props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link}   />
+            }
         </div>
     )
 }
