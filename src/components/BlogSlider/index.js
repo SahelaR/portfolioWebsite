5f20ad88eae4d8888348537d2c12e29cdfc872bd
SahelaR <sahelarani1@gmail.com>
2019-07-06 22:06:25 +0100

    scroll behaviour and grid columns adjusted

--- src/components/BlogSlider/index.js
+++ src/components/BlogSlider/index.js
@@ -8,28 +8,31 @@ export default class BlogSlider extends React.Component {
 
           <section className="section1">
           <a href="#section3">></a>
-          <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
-          <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
-          <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
-          <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+                <div className="item"><img src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all" /></div>
+                <div className="item"><img src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all" /></div>
+                <div className="item"> <img src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all" /></div>
+                <div className="item"> <img src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all" /></div>
+                <div className="item"><img src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all" /></div>
           <a href="#section2">></a>
           </section>
 
             <section className="section2">
             <a href="#section1">></a>
-                <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
-                <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
-                <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
-                <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+                <div className="item"><img src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all" /></div>
+                <div className="item"><img src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all" /></div>
+                <div className="item"><img src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all" /></div>
+                <div className="item"> <img src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all" /></div>
+                <div className="item"> <img src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all" /></div>
             <a href="#section3">></a>     
             </section>
 
             <section className="section3">
             <a href="#section2">></a>
-                <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
-                <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
-                <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
-                <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+                <div className="item"><img src="" /></div>
+                <div className="item"><img src="" /></div>
+                <div className="item"><img src="" /></div>
+                <div className="item"> <img src="" /></div>
+                <div className="item"> <img src="" /></div>
              <a href="#section1">></a>
             </section>        
       </div>
