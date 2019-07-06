cd5258807fc491b3de6d1b72f930ece4d786f461
SahelaR <sahelarani1@gmail.com>
2019-07-06 20:41:20 +0100

    placeholders added

--- src/components/BlogSlider/index.js
+++ src/components/BlogSlider/index.js
@@ -8,16 +8,28 @@ export default class BlogSlider extends React.Component {
 
           <section className="section1">
           <a href="#section3">></a>
+          <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+          <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+          <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+          <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
           <a href="#section2">></a>
           </section>
 
             <section className="section2">
             <a href="#section1">></a>
+                <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+                <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+                <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+                <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
             <a href="#section3">></a>     
             </section>
 
             <section className="section3">
             <a href="#section2">></a>
+                <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+                <div className="item"><img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+                <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
+                <div className="item"> <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY256_CR0,0,172,256_AL_.jpg" /></div>
              <a href="#section1">></a>
             </section>        
       </div>
