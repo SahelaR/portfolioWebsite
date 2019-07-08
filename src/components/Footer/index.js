34769e2d425e4028f7bcbe180890f008cf565aa9
SahelaR <sahelarani1@gmail.com>
2019-07-08 12:28:07 +0100

    copright

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import CSS from "../Footer/Footer.module.css"
 
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        <p>Footer</p>
-      </div>
+        <div className={CSS.mainCoverContainer}>
+            <div className={CSS.socialMedia}>
+
+            </div>
+            <div className={CSS.copyright}>
+                <p className={CSS.footerText}>Made in React.js</p>
+                <br />
+                <p className={CSS.footerText}>By Sahela Rani © 2019 . All Rights Reserved</p>
+
+            </div>
+
+        </div>
     )
   }
 }
