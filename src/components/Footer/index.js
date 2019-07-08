51293b330fc4a97f7246133765ab0e652ac94248
SahelaR <sahelarani1@gmail.com>
2019-07-08 14:02:35 +0100

    icon placeholders

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import CSS from "../Footer/Footer.module.css"
+import { SocialIcon } from 'react-social-icons';
+
 
 export default class Footer extends Component {
   render() {
     return (
         <div className={CSS.mainCoverContainer}>
+            
+            
             <div className={CSS.socialMedia}>
+               
+                <ul className={CSS.socialIcons}>
+                    <li> <SocialIcon network="pinterest"  bgColor="#111111" fgColor="#ffffff" style={{ height: 50, width: 50 }} /></li>
+                    <li><SocialIcon url="https://medium.com/@sahelarani1" bgColor="#111111" fgColor="#ffffff"/></li>
+                    <li><SocialIcon url="https://www.linkedin.com/in/sahelarani/" bgColor="#111111" fgColor="#ffffff"/></li>
+                    <li><SocialIcon url="https://github.com/SahelaR" fgColor="#ffffff" bgColor="#111111"/></li>
+                </ul>
+                </div>
 
-            </div>
             <div className={CSS.copyright}>
                 <p className={CSS.footerText}>Made in React.js</p>
                 <br />
