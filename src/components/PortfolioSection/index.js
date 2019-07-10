4c9adcad6e671d436360ddc0097e1167d3639e95
SahelaR <sahelarani1@gmail.com>
2019-07-10 13:22:39 +0100

    refactoring portfolio

--- src/components/PortfolioSection/index.js
+++ src/components/PortfolioSection/index.js
@@ -8,13 +8,14 @@ export default class PortfolioSection extends React.Component {
         return (
             <div>
                 <div className={CSS.mainCoverContainer}>
-                    <p>portfolio.</p>
+                    <h3 className={CSS.portfolio}>portfolio.</h3>
                     <ul className={CSS.filter}>
-                        <li>ALL</li>
-                        <li>MOBILE</li>
-                        <li>WEB</li>
+                        <li><a className={CSS.tabBtn}>ALL</a></li>
+                        <li><a className={CSS.tabBtn}>MOBILE</a></li>
+                        <li><a className={CSS.tabBtn}>WEB</a></li>
                     </ul>
                    <Carousel />
+            
                 </div>
             </div>
         )
