a1ec5ff777ef06f21bba37a567a82782ee2c043a
SahelaR <sahelarani1@gmail.com>
2019-07-10 15:45:05 +0100

    text made more consistent

--- src/App/App.js
+++ src/App/App.js
@@ -19,9 +19,9 @@ function App() {
 <LandingSection />
 <AboutMe />
 <PortfolioSection />
-
+<TechnologySection />
 <BlogSection />
-
+<ContactMe />
 <Footer />
 
       </div>
--- src/components/TechnologySection/index.js
+++ src/components/TechnologySection/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import CSS from "../TechnologySection/TechnologySection.module.css"
-import Hexagon from "../Hexagon"
+
 
 export default class TechnologySection extends React.Component {
     render() {
         return (
-<div className={CSS.mainCoverContainer}>
-<Hexagon />
+                <div>
+<div className={CSS.mainContainer}>
+<h3 className={CSS.technology}>technology.</h3>
+</div>
 </div>
         )}
 }
