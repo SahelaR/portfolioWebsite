7a38298987fb530a191f71cc2d774f2aae8cd568
SahelaR <sahelarani1@gmail.com>
2019-07-18 11:48:29 +0100

    all icons added

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -7,6 +7,11 @@ import Github from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/g
 import Nodejs from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/nodejs.svg"
 import Photoshop from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/photoshop.svg"
 import Illustrator from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/illustrator.svg"
+import Reactjs from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/reactjs.svg"
+import Mongodb from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/mongodb.svg"
+import Zenhub from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/zenhub.svg"
+import Jest from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/jest.svg"
+import Gatsby from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/gatsby.svg"
 
 
 export default class Icon extends React.Component {
@@ -19,20 +24,15 @@ export default class Icon extends React.Component {
                 <li><div className={Style.icon}><img className={Style.image} src={HTML} alt="HTML"></img></div></li>
                         <li><div className={Style.icon}><img className={Style.image} src={CSS} alt="CSS"></img></div></li>
                         <li><div className={Style.icon}><img className={Style.image} src={Javascript} alt="Javascript"></img></div></li>
-
-                <li>React.js</li>
-                <li>React-Native</li>
-                <li>Mongodb</li>
-
+                        <li><div className={Style.icon}><img className={Style.image} src={Reactjs} alt="Reactjs"></img></div></li> 
                         <li><div className={Style.icon}><img className={Style.image} src={Github} alt="Github"></img></div></li>
                 <li><div className={Style.icon}><img className={Style.image}src={Nodejs} alt="Node.js"></img></div></li>
-                
-                <li>ZenHub</li>
-                <li>Jest</li>
-
-
+              <li><div className={Style.icon}><img className={Style.image} src={Zenhub} alt="zenhub"></img></div></li>
+              <li><div className={Style.icon}><img className={Style.image} src={Jest} alt="Jest"></img></div></li>
                         <li><div className={Style.icon}><img className={Style.image} src={Illustrator} alt="Adobe Illustrator"></img></div></li>
                         <li><div className={Style.icon}><img className={Style.image} src={Photoshop} alt="Adobe Photoshop"></img></div></li>
+                         <li><div className={Style.icon}><img className={Style.image} src={Mongodb} alt="Mongodb"></img></div></li>
+              <li><div className={Style.icon}><img className={Style.image} src={Gatsby} alt="gatsby"></img></div></li>
              </ul>
              
               </div>
