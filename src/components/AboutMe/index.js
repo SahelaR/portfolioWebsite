99e73c8991bce3b43237e47009459638f5ae1399
SahelaR <sahelarani1@gmail.com>
2019-07-20 20:53:09 +0100

    webkit

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -4,20 +4,23 @@ import Background from "/Users/sahel/Documents/portfolioWebsite/src/Assets/Image
 
 
 export default class AboutMe extends React.Component {
+
+
   render() {
     return (
 
-        <div className={CSS.mainCoverContainer} style={{  backgroundSize: "cover",
-            backgroundAttachment: 'fixed',
-          backgroundRepeat: 'no-repeat', zIndex: 1, backgroundImage: "url(" + Background + ")" }}>
+        <div className={CSS.mainCoverContainer}>
          <div className={CSS.header}>
             <h3 className={CSS.about}>about.</h3>
+            <br />
           <hr style={{width: "10% ", alignItems: "left"}} />
-          <br />
-          <p className={CSS.paragraph}><strong >law student is taking a structure & detailed-orientated approach
-            to coding,</strong> building both websites & mobile applications. after a 16 week coding bootcamp, react.js and react-native 
-            are my now current favourite tools, so looking for my next project - so get in contact.<br /> <br /></p><p className={CSS.paragraph} style={{ textAlign: "center"}}>tuning into perspective & turning out code.</p>
+          <p className={CSS.paragraph}>law student is taking a structure & detailed-orientated approach
+            to coding, building both websites & mobile applications. after a 16 week coding bootcamp, react.js and react-native 
+            are my now current favourite tools, so looking for my next project - so get in contact.</p>
+            <br />
+            <p className={CSS.paragraph} style={{ textAlign: "center"}}>tuning into perspective & turning out code.</p>
           <hr style={{ width: "10% ", alignItems: "left" }} />
+          <br />
       </div>
         </div>
   
---
+++ src/components/Loader/index.js
@@ -0,0 +1,40 @@
+
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            number: 0
+        }
+    }
+    componentDidMount() {
+
+        let interval = setInterval(() => {
+            const { number } = this.state
+            if (number < 100) {
+                this.setState({ number: number + 1 })
+            } else {
+                interval.clearInterval()
+            }
+        }, 50)
+    }
+    render() {
+        return (
+            <div className="App">
+                <Loader number={this.state.number} />
+            </div>
+        )
+    }
+}
+
+const Loader = ({ number }) => {
+    let numberString = number
+    if (number < 10) {
+        numberString = '0' + number
+    }
+    return (
+        <div className="Loader" data-size={number}>{numberString}<sup className="precent">%</sup>
+        </div>
+    )
+}
+
