0f1f38edb5c5248e5d4bbf2cd4b4cc9728a9a4a9
SahelaR <sahelarani1@gmail.com>
2019-07-23 14:00:18 +0100

    remove npm package

--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import CSS from "../SocialMedia/SocialMedia.module.css";
-import { SocialIcon } from "react-social-icons";
+
 
 export default class SocialMedia extends React.Component {
     render() {
         return (
                 <ul className={CSS.contactList}>
                     <p className={CSS.subtitle}>stay connected</p>
-                    <li><SocialIcon url="https://twitter.com/sxhelar" bgColor="#331832" /> </li>
-                    <li><SocialIcon url="https://medium.com/@sahelarani1" bgColor="#331832" /></li>
-                    <li><SocialIcon url="https://www.linkedin.com/in/sahelarani/" bgColor="#331832" /></li>
-                    <li><SocialIcon url="https://github.com/SahelaR" bgColor="#331832" /></li>
+                    
                 </ul>
             
         );
