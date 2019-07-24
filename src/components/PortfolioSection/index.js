223f9884c347889d6d25cc69989528c77124b6e3
SahelaR <sahelarani1@gmail.com>
2019-07-24 12:17:34 +0100

    title css

--- src/components/PortfolioSection/index.js
+++ src/components/PortfolioSection/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CSS from "../PortfolioSection/PortfolioSection.module.css"
-import Carousel from "../Carousel/index"
+
+import BrumGo from "/Users/sahel/Documents/portfolioWebsite/src/Assets/Images/brumGoLogoRed.svg"
 
 
 export default class PortfolioSection extends React.Component {
@@ -11,9 +12,42 @@ export default class PortfolioSection extends React.Component {
                     <div className={CSS.mainContent}>
                         <div  className={CSS.portfolioCont}>
                             <div className={CSS.portfolioTitle + ' ' + CSS.one}><span className={CSS.portfolio}>portfolio.</span></div>
-                            <div className={CSS.portfolioItem + ' ' + CSS.two}>brumgo</div>
-                            <div className={CSS.portfolioItem + ' ' + CSS.three}>SAIZ</div>
-                            <div className={CSS.portfolioItem + ' ' + CSS.four }>Talent Growth</div>
+                            <div className={CSS.portfolioItem + ' ' + CSS.two}>
+                                <div className={CSS.titlePrimary}>
+                                    <h2 className={CSS.app}>BrumGo</h2>
+                                    <p clssName={CSS.subText}>a city guide app</p>
+                                    <img className={CSS.brumgo} src={BrumGo} alt="BrumGo"></img>
+                                </div>
+                                 {/* <div className={CSS.titleSecondary}>
+                                    <p>click on the logo to read more</p> 
+                                </div> */}
+                            </div>
+                           
+                            <div className={CSS.portfolioItem + ' ' + CSS.three}>
+                                <div className={CSS.titlePrimary}>
+                                    <h2 className={CSS.app}>Talent Growth</h2>
+                                    <p clssName={CSS.subText}>a city guide app</p>
+                                    <img className={CSS.brumgo} src={BrumGo} alt="BrumGo"></img>
+                                </div>
+                                {/* <div className={CSS.titleSecondary}>
+                                    <p>click on the logo to read more</p> 
+                                </div> */}
+
+                            </div>
+
+
+                            
+                            <div className={CSS.portfolioItem + ' ' + CSS.four }>
+                                <div className={CSS.titlePrimary}>
+                                    <h2 className={CSS.app}>SAIZ</h2>
+                                    <p clssName={CSS.subText}>a city guide app</p>
+                                    <img className={CSS.brumgo} src={BrumGo} alt="BrumGo"></img>
+                                </div>
+                                {/* <div className={CSS.titleSecondary}>
+                                    <p>click on the logo to read more</p> 
+                                </div> */}
+
+                            </div>
                         </div>
                     </div>
                 </div>
