c9e141c90e5f4338b2c7d1e8de14158d4998081e
SahelaR <sahelarani1@gmail.com>
2019-09-12 19:46:46 +0100

    removed files

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -23,7 +23,6 @@ export default class AboutMe extends React.Component {
           <p className={CSS.paragraph}>law student is taking a structure & detailed-orientated approach
             to coding, building both websites & mobile applications. after a 16 week coding bootcamp, react.js and react-native 
             are my now current favourite tools, so looking for my next project - so get in contact.</p>
-            <br />
             <p className={CSS.paragraph} style={{ textAlign: "center"}}>tuning into perspective & turning out code.</p>
           <hr style={{ width: "10% ", alignItems: "left" }} />
           <br />
