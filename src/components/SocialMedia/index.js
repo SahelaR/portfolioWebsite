681a988e9e8684ed4b5d2eed2ad0d8fa541b9433
SahelaR <sahelarani1@gmail.com>
2019-09-12 18:56:41 +0100

    src adjustment

--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CSS from "../SocialMedia/SocialMedia.module.css";
-
+import Linkedin from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/linkedin.svg"
+import Twitter from "/Users/sahel//Documents/portfolioWebsite/src/Assets/Images/twitter.svg"
 
 export default class SocialMedia extends React.Component {
     render() {
@@ -10,16 +11,9 @@ export default class SocialMedia extends React.Component {
                
                
                 <div class="section" />
-                    <a href="https://www.facebook.com/UsmanManiDar" target="_blank"><img class="fb" src="https://goo.gl/I5LtpV" alt="Facebook" width="48px" height="48px" /></a>
+                <a href="https://www.linkedin.com/in/sahelarani/"><img class="fb" src={Linkedin} alt="Facebook" width="48px" height="48px" /></a>
 
-                        <a href="https://twitter.com/UsmanManiDar" target="_blank"><img class="twitter" src="https://goo.gl/iph3IL" alt="Twitter" width="48px" height="48px" /></a>
-
-                            <a href="#" target="_blank"><img class="insta" src="https://goo.gl/PDgUIa" alt="Instagram" width="48px" height="48px" /></a>
-
-                                <a href="#" target="_blank"><img class="pin" src="https://goo.gl/0eCZlG" alt="Pinterest" width="48px" height="48px" /></a>
-
-                                    <a href="#" target="_blank"><img class="google" src="https://goo.gl/SLSMhg" alt="Google +" width="48px" height="48px" /></a>
-
+                <a href="https://twitter.com/sxhelar" ><img class="twitter" src={Twitter} alt="Twitter" width="48px" height="48px" /></a>
 
                 </div>
 
